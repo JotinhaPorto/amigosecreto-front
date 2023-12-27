7341c60f0ccc1c03d682d673d46422ec8691cd02
João Pedro Braga Porto <joao.braga.porto01@gmail.com>
2023-12-27 07:24:58 -0300

    ddd

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,4 +1,5 @@
 import * as api from '@/app/api/site'
+import { redirect } from 'next/navigation'
 
 type Props = { params: { id: string } }
 
@@ -6,11 +7,20 @@ const page = async ({ params }: Props) => {
 
     const event = await api.getEvent(parseInt(params.id))
 
+    if (!event) {
+        return redirect('/')
+    }
+
     return (
-        <div>
-            {JSON.stringify(event)}
-            
-        </div>
+        <main className='max-w-lg mx-auto text-white text-center p-4'>
+            <div>
+                <h1 className='text-3xl font-bold text-blue-400'>Amigo secreto</h1>
+                <h2 className='text-2xl mt-5 mb-2'>{event.title}</h2>
+                <p>{event.Description}</p>
+            </div>
+
+            ...
+        </main>
     )
 }
 
